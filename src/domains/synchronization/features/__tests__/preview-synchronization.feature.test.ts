4a58826d619275862a4bb90ced77449b0e6f5bf1
Myastr0 <14167316+Myastr0@users.noreply.github.com>
2025-12-19 16:27:59 +0100

    fix(fix test file): fix test file

--- src/domains/synchronization/features/__tests__/preview-synchronization.feature.test.ts
+++ src/domains/synchronization/features/__tests__/preview-synchronization.feature.test.ts
@@ -2,7 +2,7 @@ import { FileFixture } from '../../../../../__tests__/__fixtures__/file.fixture'
 import { FakeSourceRepository } from '../../../../../__tests__/__fakes__/synchronization/fake-source.repository';
 import { SiteMap } from '../../../sitemap';
 import { PreviewSynchronization } from '../preview-synchronization.feature';
-import { FakeEventLoggerRepository } from '__tests__/__fakes__/event-logs/fake-event-logger.repository';
+import { FakeEventLoggerRepository } from '../../../../../__tests__/__fakes__/event-logs/fake-event-logger.repository';
 
 describe('PreviewSynchronization', () => {
   let previewSync: PreviewSynchronization<any>;
